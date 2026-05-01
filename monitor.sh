82717ec5e55c5764839b40c2f65b2b8e82be2d90
Eljerr <eliazerraditya@gmail.com>
2026-05-01 09:34:17 +0700

    feat: tambah alert sistem jika disk mencapai 80% pemkaian

--- monitor.sh
+++ monitor.sh
@@ -4,6 +4,11 @@ cek_resource() {
   echo "$(date)"
 
   df -h / | awk 'NR==2 {print "Total disk:", $2 , "| Used:", $3 , "| Free space:", $4}'
+
+  diskusage=$(df -h / | awk 'NR==2 {print $5}' | tr -d "%")
+  if [[ $diskusage -gt 80 ]]; then
+    echo "[ALERT] Disk uang terpakai sudah di atas 80%"
+  fi
 }
 
 cek_resource
